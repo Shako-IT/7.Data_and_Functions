3352ee254ad72925e131cfc03a38742f70758da0
Shalva Mghebrishvili <shmgeo88@gmail.com>
2022-10-23 22:15:52 +0400

    Finished

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="visibility">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleVisibility">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +13,46 @@
 </template>
 
 <script>
+// import { ref } from 'vue';
+// import { reactive } from 'vue';
+
 export default {
-  
-}
+  data() {
+    return {
+      goal: 'Learn Vue',
+      visibility: false,
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.visibility = !this.visibility;
+    },
+  },
+  // setup() {
+  //   // const coursGoal = ref('Learn Vue');
+  //   // const visible = ref(false);
+  //   // const coursData = ref({
+  //   //   goal: 'Learn Vue',
+  //   //   visibility: false,
+  //   // });
+  //   const coursData = reactive({
+  //     goal: 'Learn Vue',
+  //     visibility: false,
+  //   });
+  //   function toggleVisibility() {
+  //     // visible.value = !visible.value;
+  //     // coursData.value.visibility = !coursData.value.visibility;
+  //     coursData.visibility = !coursData.visibility;
+  //   }
+  //   return {
+  //     // goal: coursData.value.goal,
+  //     goal: coursData.goal,
+  //     // visible: visible,
+  //     coursData: coursData,
+  //     toggleVisibility: toggleVisibility,
+  //   };
+  // },
+};
 </script>
 
 <style>
